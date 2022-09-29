789f622cedc0677b8ff95c4c63af6e2f18050ac3
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-29 16:25:49 +0700

    Update download-ytv1080.js

--- plugins/download-ytv1080.js
+++ plugins/download-ytv1080.js
@@ -53,7 +53,7 @@ handler.menudownload = ['ytvideo1080 <url>']
 handler.tagsdownload = ['search']
 handler.command = /^(yt(v(ideo)?|mp4)1080p?)$/i
 
-handler.premium = true
+handler.premium = false
 handler.limit = true
 
 export default handler
