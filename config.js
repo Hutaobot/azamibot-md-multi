ebce498f210dc4187615d9b5745f5b26b510267d
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-29 13:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['6285282250251'],
+	['6283854551575'],
 	['79309965233'],
-	['6282337245566', 'Syahrul A.S', true]
+	['6283854551575', 'A7 RAFLI', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -32,8 +32,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-/*global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'*/
+/*global.packname = 'wa.me/6285722157719'
+global.author = 'HU TAO BOT-MD'*/
 
 global.multiplier = 69 // The higher, The harder levelup
 
